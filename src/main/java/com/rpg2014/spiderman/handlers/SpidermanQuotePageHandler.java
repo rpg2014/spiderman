fbfd5be7ba8578a8f4feb631e127952950d7c873
rpg2014 <rpg2014@users.noreply.github.com>
2019-01-09 09:49:07 -0800

    forgot )

--- src/main/java/com/rpg2014/spiderman/handlers/SpidermanQuotePageHandler.java
+++ src/main/java/com/rpg2014/spiderman/handlers/SpidermanQuotePageHandler.java
@@ -115,7 +115,7 @@ public class SpidermanQuotePageHandler implements HttpHandler {
 		if (quoteWrapper == null) {
 			quoteWrapper = SpidermanQuoteWrapper.getInstance();
 		}
-		logger.logInfo("Adding quote: "+params.get("quote").substring(0, Math.max(20,params.get("quote").length()+1)+"... by: "+params.get("name"), className);
+		logger.logInfo("Adding quote: "+params.get("quote").substring(0, Math.max(20,params.get("quote").length()+1))+"... by: "+params.get("name"), className);
 		quoteWrapper.addQuote(new Person(params.get("name")), params.get("quote").trim());
 		
 	}
